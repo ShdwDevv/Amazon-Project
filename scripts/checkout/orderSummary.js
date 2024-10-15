f9ab54a58fc588a7c4ece739c5164b03cfa7cb64
arshathahamed10 <arshathahamed500@gmail.com>
2024-10-15 16:22:49 +0530

    make a class for product nad make changes in code

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -31,7 +31,7 @@ export function renderOrderSummary(){
                 ${matchedProduct.name}
             </div>
             <div class="product-price">
-                $${formatCurrency(matchedProduct.priceCents)}
+                ${matchedProduct.getPrice()}
             </div>
             <div class="product-quantity">
                 <span>
