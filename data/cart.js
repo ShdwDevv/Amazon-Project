2a55963278e55e45e8248abf06f2ed5e602da21c
arshathahamed10 <arshathahamed500@gmail.com>
2024-10-16 13:19:40 +0530

    Convert loadProduct to LoadProductFetch

--- data/cart.js
+++ data/cart.js
@@ -57,3 +57,13 @@ export function updateDeliveryOption(productId,deliveryOptionId){
     matchingItem.deliveryOptionId = deliveryOptionId;
     saveStorage();
 }
+
+export function loadCart(fun){
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+    fun();
+  })
+  xhr.open('GET','https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,69 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
-import {loadProducts} from '../data/products.js';
+import {loadProducts,loadProductsFetch} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
+
+Promise.all([
+    // new Promise((resolve) => {
+    //     loadProducts(()=>{
+    //         resolve('one');
+    //     });
+    // })
+    loadProductsFetch(),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then((val) => {
+    console.log(val);
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+/*
+// Added cart with promise
+new Promise((resolve) => {
+    loadProducts(()=>{
+        resolve();
+    });
+}).then(()=>{
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+}).then(() =>{
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+*/
+/*
+// Added Cart
+loadProducts(()=>{
+    loadCart(()=>{
+        renderOrderSummary();
+        renderPaymentSummary();
+    })
+})
+*/
+/*
+// Using Promise
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('store in val');
+    })
+}).then((val) => {
+    console.log(val);
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+*/
+// Using call back
+/*
 loadProducts(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
